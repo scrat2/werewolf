52240a00d87e25d7a8e1f1fcd60c164baf9ef22b
scrat2 <boussignac.s@gmail.com>
2020-04-20 15:45:16 +0200

    Receives read request and answer to the client

--- server/Network_Listener.java
+++ server/Network_Listener.java
@@ -1,12 +1,22 @@
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Network_Listener {
+    //Create request reader and answerer
+    private static BufferedInputStream enterRequest = null;
+    private static PrintWriter answer = null;
 
     public static void main(String[] args) {
-
+        //create sockets
         ServerSocket server = null;
+        Socket client = null;
+
+        //Run the Network Listener
         while (true) {
+            //try to create the socket on port 4444
             try {
                 server = new ServerSocket(4444);
             } catch (IOException e) {
@@ -14,14 +24,30 @@ public class Network_Listener {
                 System.out.println("Port used");
             }
 
+            //Wait for a connexion read the request and answer to this one
             try {
-                server.accept();
+                //wait
+                client = server.accept();
+
+                //Connection received and read request
                 System.out.println("Client connection received.");
+                System.out.println("Client IP : "+ client.getInetAddress());
+                enterRequest = new BufferedInputStream(client.getInputStream());
+                String enterRequestContent = read();
+                System.out.println(enterRequestContent);
+
+                //answer to this request
+                answer = new PrintWriter(client.getOutputStream(), true);
+                answer.write("reçu 5/5");
+                answer.flush();
+                System.out.println("réponse correctement envoyé");
             } catch (IOException e) {
                 e.printStackTrace();
+                e.getMessage();
                 System.out.println("error Connexion client.");
             }
 
+            //close the socket to wait an other connection
             try {
                 server.close();
             } catch (IOException e) {
@@ -29,6 +55,15 @@ public class Network_Listener {
                 server = null;
             }
         }
+    }
 
+    //Reader method translate byte in string
+    private static String read() throws IOException{
+        String response = "";
+        int stream;
+        byte[] b = new byte[4096];
+        stream = enterRequest.read(b);
+        response = new String(b, 0, stream);
+        return response;
     }
 }
