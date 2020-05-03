a0cf94c9b9297a6288ff27ad4a27a273fefd078f
LucasP <lphilippe.ing2022@esaip.org>
2020-05-03 22:50:23 +0200

    Add of the subclass witcher

---
+++ client/witcher.java
@@ -0,0 +1,19 @@
+package werewolf;
+
+/**
+ *
+ * @author lucas
+ */
+public class witch {
+    
+    String resurrected_person;
+    String poisoned_person;
+    
+    public boolean save(String pseudo){// method for resurrected the people dead during the night
+        return true;
+    }
+    
+    public boolean poison(String pseudo){// method for kill 1 people during the night
+        return false;
+    }
+}
