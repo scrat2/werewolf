c5b54cd2356492fd96ea31a53e0f61cb4ac5e8a6
LucasP <lphilippe.ing2022@esaip.org>
2020-05-09 15:21:23 +0200

    Modification of cupidon class and add array lovers

--- client/cupidon.java
+++ client/cupidon.java
@@ -6,33 +6,19 @@ package werewolf;
  */
 public class cupidon extends player {
     
-    String lover1, lover2;
-    
-    public String love(){ // method for choose the lovers and linked them
+        
+    public String[] love(){ // method for choose the lovers and linked them
+        
+        String[] lovers= new String[2];
+        
         System.out.println("Which citizen will be the first lover ?");
-        lover1 = lectureClavier.nextLine();
+        lovers[0] = lectureClavier.nextLine();
         
         System.out.println("Which citizen will be the second lover ?");
-        lover2 = lectureClavier.nextLine();
+        lovers[2] = lectureClavier.nextLine();
         
-        return lover1 + lover2;// return the two lovers
+        return lovers;// return the two lovers
         
     }
     
-public class cupidon extends player {
-
-    String lover1, lover2;
-
-    public String love(){ // method for choose the lovers and linked them
-        System.out.println("Which citizen will be the first lover ?");
-        lover1 = lectureClavier.nextLine();
-
-        System.out.println("Which citizen will be the second lover ?");
-        lover2 = lectureClavier.nextLine();
-
-        return lover1 + lover2;// return the two lovers
-
-    }
-
-}
 }
