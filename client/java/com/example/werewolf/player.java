4a242eba08b801f5485576c09bd771ea6f99c1fc
LucasP <lphilippe.ing2022@esaip.org>
2020-05-16 21:24:39 +0200

    Update

--- client/java/com/example/werewolf/player.java
+++ client/java/com/example/werewolf/player.java
@@ -1,19 +1,41 @@
 package com.example.werewolf;
 
 //player object uses to store data about player
+import java.util.Scanner;
+
+
 public class player {
-    String pseudo;
-    String role;
 
-    public player() {
+
+    Scanner lectureClavier = new Scanner(System.in);
+    String pseudo; //name is the game
+    String role; //role during the game
+    String choicevote;
+    Boolean life = true;
+
+    public player(){
+
     }
-
-    public player(String pseudo) {
+    public  player(String pseudo){
         this.pseudo = pseudo;
     }
 
-    public player( String pseudo, String role) {
+    public player(String pseudo, Boolean life){
         this.pseudo = pseudo;
+        this.life = life;
+    }
+
+    public player(String pseudo, Boolean life, String role){
+        this.pseudo = pseudo;
+        this.life = life;
         this.role = role;
     }
+
+    public String vote(){ //method for vote with the citizen during the day
+        System.out.println("Which citizen do you want eliminate ?");
+        choicevote = lectureClavier.nextLine();
+        return choicevote;
+    }
+
 }
+
